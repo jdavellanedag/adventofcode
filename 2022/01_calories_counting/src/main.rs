722b3b3b9e40dbe6264ca2ea25085acf5c22997f
jdavellanedag <jdavellanedag@gmail.com>
2023-06-25 19:25:00 +0200

    Read file method improved

--- 2022/01_calories_counting/src/main.rs
+++ 2022/01_calories_counting/src/main.rs
@@ -2,8 +2,11 @@ use std::fs::read_to_string;
 
 // Disclaimer, this is a ugly piece of code, I'm trying to learn tmux + vim + rust at the same time
 fn main() {
-    let data = read_lines("./input.txt");
-
+   
+    let data = read_lines("./input.txt").unwrap_or_else(|err| {
+        panic!("Failed to read input file: {}", err);
+    });
+    
     let mut sum: u64 = 0;
     let mut total = Vec::new();
 
@@ -20,11 +23,9 @@ fn main() {
     println!("Elfe with more food is: {}", total.iter().max().unwrap());
 }
 
-fn read_lines(filename: &str) -> Vec<String> {
-    let mut result = Vec::new();
-    for line in read_to_string(filename).unwrap().lines() {
-        result.push(line.to_string())
-    }
-
-    result
+fn read_lines(filename: &str) -> Result<Vec<String>, std::io::Error> {
+    read_to_string(filename)?
+        .lines()
+        .map(|line| Ok(line.to_string()))
+        .collect()
 }
