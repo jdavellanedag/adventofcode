545e866960e1953d8d69c7c1c29ab492545b1786
jdavellanedag <jdavellanedag@gmail.com>
2023-06-29 17:49:03 +0200

    Fix gitignore

--- 2022/02_rock_paper_scissors/target/debug/deps/rock_paper_scissors-bb7d088cbc2fa0c0.d
+++
@@ -1,5 +0,0 @@
-/home/jdavellaneda/projects/adventofcode/2022/02_rock_paper_scissors/target/debug/deps/rock_paper_scissors-bb7d088cbc2fa0c0: src/main.rs
-
-/home/jdavellaneda/projects/adventofcode/2022/02_rock_paper_scissors/target/debug/deps/rock_paper_scissors-bb7d088cbc2fa0c0.d: src/main.rs
-
-src/main.rs:
--- 2022/02_rock_paper_scissors/target/debug/rock_paper_scissors.d
+++
@@ -1,1 +0,0 @@
-/home/jdavellaneda/projects/adventofcode/2022/02_rock_paper_scissors/target/debug/rock_paper_scissors: /home/jdavellaneda/projects/adventofcode/2022/02_rock_paper_scissors/src/main.rs
